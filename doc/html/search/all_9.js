af557e0b144f44e1d080d22e67ab1371369c060a
Alex Hoffman <alxhoff@gmail.com>
2017-11-09 17:18:09 +0100

    ops removing doxygen documentation again

--- doc/html/search/all_9.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['predic',['predic',['../structdatalog__query__answer.html#a721776aabf590192c96df972333d38d4',1,'datalog_query_answer']]],
-  ['predicate',['predicate',['../structdatalog__literal.html#aad44475a0e606d9fd9c5e7aae3667412',1,'datalog_literal::predicate()'],['../structdl__parser__literal.html#a29b6704d93332f103bd46525b651c62e',1,'dl_parser_literal::predicate()']]],
-  ['processed_5fanswer',['processed_answer',['../structdatalog__query.html#a54e2eef2c18bc5e4c0f0a6c10f1e6397',1,'datalog_query']]]
-];
--- doc/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['datalog_20api',['Datalog API',['../md__home_alxhoff_git_GitHub_Datalog-API_README.html',1,'']]]
-];
